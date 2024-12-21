ff5c20548fdc8cb56cdbf01d58db37fbfb63cc33
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-12-21 11:06:35 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+                           # Arithmetic Operators 
+
+value1: int = 10
+value2: int = 5
+
+print(value1 + value2)  # Addition
+print(value1 - value2)  # Subtraction
+print(value1 / value2)  # Divison
+print(value1 * value2)  # Multiplication
+print(value1 ** value2) # Exponent
+print(value1 // value2) # Floor Division
+print(value1 % value2)  # Modulus
+ 
+ 
+                           # Assigment Operators
+                           
+                           
+sum: int = 10          
+sum = sum + 5           # Equal   
+ 
+print(sum)                           
+                  
+                           
+                           
+                           # Comparison Operators
+                           
+first_No: int = 10
+second_No: int = 6
+
+print(first_No == second_No)  # Equal
+print(first_No > second_No)   # Greator Than
+print(first_No < second_No)   # Less Than
+print(first_No >= second_No)  # Greator or Equal to
+print(first_No <= second_No)  # Less or Equal to
+print(first_No != second_No)  # Not Equal to      
+
+
+
+                            
+                              # Logical Operators
+                              
+# and , or , not                                                  
